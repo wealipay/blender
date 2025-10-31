873a86cd3b4018834dfb30a56fddedf0ec94f098
wealipay <1293600794@qq.com>
2025-10-31 14:26:49 +0800

    hhn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import Footer from '@/components/Footer';
+import NavBar from '@/components/NavBar';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="mx-auto sm:max-w-xl"> {children}</div>
+        <div className="mx-auto p-4 sm:max-w-xl md:max-w-2xl lg:max-w-3xl xl:max-w-6xl">
+         <NavBar />
+  {children}
+  <Footer />
+
+        </div>
       </body>
     </html>
   );
