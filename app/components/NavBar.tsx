9671dcce124eee8ef4af99ba6314a1a2457ddc84
wealipay <1293600794@qq.com>
2025-10-31 15:43:52 +0800

    hhh

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -9,6 +9,7 @@ const NavBar = () => {
           src="/logo.png"
           width={36}
           height={36}
+          alt="logo"
           className="w-6 h-6 md:w-9 md:h-9"
         />
       </Link>
