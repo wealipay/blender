1f514973d415597ffbffb8e69a5557640dea321c
wealipay <1293600794@qq.com>
2025-10-31 22:35:13 +0800

    hhh

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,11 @@
+import SearchBar from "./SearchBar";
 import Image from "next/image";
 import Link from "next/link";
+import {Home} from "lucide-react"
 
 const NavBar = () => {
   return (
-    <nav className="w-full flex items-center justify-between border-b border-gray-200">
+    <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4">
       {/*Left*/}
       <Link href="/" className="flex items-center">
         <Image
@@ -13,10 +15,17 @@ const NavBar = () => {
           alt="logo"
           className="w-6 h-6 md:w-9 md:h-9"
         />
-        <p className="text-md font-medium tracking-wider">Wealipay</p>
+        <p className="hidden md:block text-md font-medium tracking-wider">
+          Wealipay
+        </p>
       </Link>
       {/*Right*/}
-      <div className="">Right</div>
+      <div className="">
+        <SearchBar />
+        <Link href="/">
+          <Home />
+        </Link>
+      </div>
     </nav>
   );
 };
