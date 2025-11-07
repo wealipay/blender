1cd9733582420facb9f47fca5b038b05ef530bdf
wealipay <1293600794@qq.com>
2025-11-07 08:15:09 +0800

    hhh

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,48 @@
-const Footer=()=>{
-  return <div>Footer</div>
-}
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="mt-16 flex-col items-center md:flex-row md:items-start md:justify-between bg-gray-800 p-8 rounded-lg">
+      <div className="flex flex-col gap-4 items-center md:items-start">
+        <Link href="/" className="flex items-center">
+          <Image src="/logo.png" width={36} height={36} alt="logo" />
+          <p className="hidden md:block text-md font-medium tracking-wider text-white">
+            Wealipay
+          </p>
+        </Link>
+        <p className="text-sm text-gray-400">© 2025 Wealipay</p>
+        <p className="text-sm text-gray-400">版权所有</p>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">相关链接</p>
+        <Link href="/">首页</Link>
+        <Link href="/">服务</Link>
+        <Link href="/">关于</Link>
+        <Link href="/">领红包</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">相关链接</p>
+        <Link href="/">首页</Link>
+        <Link href="/">服务</Link>
+        <Link href="/">关于</Link>
+        <Link href="/">领红包</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">相关链接</p>
+        <Link href="/">首页</Link>
+        <Link href="/">服务</Link>
+        <Link href="/">关于</Link>
+        <Link href="/">领红包</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">相关链接</p>
+        <Link href="/">首页</Link>
+        <Link href="/">服务</Link>
+        <Link href="/">关于</Link>
+        <Link href="/">领红包</Link>
+      </div>
+    </div>
+  );
+};
 export default Footer;
