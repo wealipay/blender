0bde7084cff6cb95ec9a51c278a1f51dbdc06671
wealipay <1293600794@qq.com>
2025-11-13 21:22:48 +0800

    fff

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,6 +1,67 @@
 "use client";
+import {
+  Footprints,
+  Glasses,
+  Briefcase,
+  Shirt,
+  ShoppingBasket,
+  Hand,
+  Venus
+} from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-const Categories=()=>{
-  return <div></div>
-}
-export default Categories
+const categories = [
+  {
+    name: "All",
+    icon: <ShoppingBasket className="w-4 h-4" />,
+    slug: "all"
+  },
+  {
+    name: "T-shirts",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "t-shirts"
+  },
+  {
+    name: "Shoes",
+    icon: <Footprints className="w-4 h-4" />,
+    slug: "shoes"
+  },
+  {
+    name: "Accessories",
+    icon: <Glasses className="w-4 h-4" />,
+    slug: "accessories"
+  },
+  {
+    name: "Bags",
+    icon: <Briefcase className="w-4 h-4" />,
+    slug: "bags"
+  },
+  {
+    name: "Dresses",
+    icon: <Venus className="w-4 h-4" />,
+    slug: "dresses"
+  },
+  {
+    name: "Jackets",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "jackets"
+  },
+  {
+    name: "Gloves",
+    icon: <Hand className="w-4 h-4" />,
+    slug: "gloves"
+  }
+];
+const Categories = () => {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 rounded-lg p-2 mb-4 text-sm">
+      {categories.map(category => (
+        <div className="flex justify-center items-center gap-2 rounded-md cursor-pointer px-2 py-1 " key={category.name}>
+          {category.icon}
+          {category.name}
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Categories;
