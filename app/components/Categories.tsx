d93ea299a1c7a2376c60d36891fb603eb4eaeb75
wealipay <1293600794@qq.com>
2025-12-03 23:12:11 +0800

    hhj

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -53,10 +53,16 @@ const categories = [
   }
 ];
 const Categories = () => {
+  const searchParams = useSearchParams();
+  const selectedCategory = searchParams.get("category");
+
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 rounded-lg p-2 mb-4 text-sm">
       {categories.map(category => (
-        <div className="flex justify-center items-center gap-2 rounded-md cursor-pointer px-2 py-1 " key={category.name}>
+        <div
+          className={`flex justify-center items-center gap-2 rounded-md cursor-pointer px-2 py-1 ${categories.slug===selectedCategory ? "bg-white" :"text-gray-500"}`}
+          key={category.name}
+        >
           {category.icon}
           {category.name}
         </div>
