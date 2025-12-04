61a9f1dd8602af5132f594b2eae370280c08fb47
wealipay <1293600794@qq.com>
2025-12-04 21:27:53 +0800

    hhh

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -52,9 +52,8 @@ const categories = [
     slug: "gloves"
   }
 ];
-const Categories = () => {
-  const searchParams = useSearchParams();
-  const selectedCategory = searchParams.get("category");
+const Categories = ({searchParams}) => {
+  const selectedCategory = searchParams?.category;
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 rounded-lg p-2 mb-4 text-sm">
